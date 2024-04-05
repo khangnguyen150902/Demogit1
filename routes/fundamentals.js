8d928c81e192fe17c53578f57b57811c8b5deb83
Kane <khangnguyen150902@gmail.com>
2024-04-05 09:02:44 +0700

    initial commit

--- routes/fundamentals.js
+++ routes/fundamentals.js
@@ -12,200 +12,223 @@ router.get('/', function (req, res, next) {
     });
 });
 
-function sum2number(a, b) {
+router.get('/sum-two-number', function (req, res, next) {
+    res.send({
+        name: 'Sum',
+        server: 'express',
+        sum: sumTwoNumbers(1, 2)
+    });
+});
+
+function sumTwoNumbers(a, b) {
     return a + b;
 }
-router.get('/sum2number', function (req, res, next) {
+
+router.get('/comparison-operators', function (req, res, next) {
     res.send({
-        name: 'sum2number',
-        sum: sum2number(1, 2)
+        name: 'Comparison operators',
+        server: 'express',
+        compare: comparisonOperators(2, 2)
     });
 });
 
-function f2(a, b) {
+function comparisonOperators(a, b) {
     return a === b;
 }
-router.get('/f2', function (req, res, next) {
+
+router.get('/get-typeof-value', function (req, res, next) {
     res.send({
-        name: 'f2',
-        sum: f2(5, 5)
+        name: 'Get type of value',
+        server: 'express',
+        typeofValue: getTypeofValue('Test')
     });
 });
 
-function f3(a) {
+function getTypeofValue(a) {
     return typeof a;
 }
-router.get('/f3', function (req, res, next) {
+
+router.get('/get-nth-character', function (req, res, next) {
     res.send({
-        name: 'f3',
-        sum: f3(9)
+        name: 'Get nth character',
+        server: 'express',
+        character: getNthCharacter('abcd', 3)
     });
 });
 
-function f4(a, n) {
+function getNthCharacter(a, n) {
     return a[n - 1];
 }
-router.get('/f4', function (req, res, next) {
+
+router.get('/remove-first-n-characters', function (req, res, next) {
     res.send({
-        name: 'f4',
-        sum: f4("abcd", 1)
+        name: 'Remove first n characters',
+        server: 'express',
+        afterRemove: RemoveFirstNcharacters('abcdefg')
     });
 });
 
-function f5(a) {
+function RemoveFirstNcharacters(a) {
     return a.slice(3);
 }
-router.get('/f5', function (req, res, next) {
+
+router.get('/get-last-n-characters', function (req, res, next) {
     res.send({
-        name: 'f5',
-        sum: f5("abcdefg")
+        name: 'Get last n characters',
+        server: 'express',
+        nCharacters: getLastNcharacters('abcdefg')
     });
 });
 
-function f6(str) {
+function getLastNcharacters(str) {
     return str.slice(-3);
 }
-router.get('/f6', function (req, res, next) {
+
+router.get('/find-position-of-string-in-another', function (req, res, next) {
     res.send({
-        name: 'f6',
-        sum: f6("abcdefg")
+        name: 'Find the position of one string in another',
+        server: 'express',
+        position: findIs('praise')
     });
 });
 
-function f7(a) {
-    return a.slice(0, 3);
+function findIs(a) {
+    return a.indexOf('is');
 }
-router.get('/f7', function (req, res, next) {
+
+router.get('/extract-first-half', function (req, res, next) {
     res.send({
-        name: 'f7',
-        sum: f7("abcdefg")
+        name: 'Extract first half of string',
+        server: 'express',
+        position: extractFirstHalf('12345678')
     });
 });
 
-function f8(a) {
-    var index = a.indexOf('is');
-    return index;
-}
-router.get('/f8', function (req, res, next) {
-    res.send({
-        name: 'f8',
-        sum: f8("risky")
-    });
-});
-
-function f9(a) {
+function extractFirstHalf(a) {
     return a.slice(0, a.length / 2);
-  }
-router.get('/f9', function (req, res, next) {
+}
+
+router.get('/remove-last-n-characters', function (req, res, next) {
     res.send({
-        name: 'f9',
-        sum: f9("1234")
+        name: 'Remove last n characters',
+        server: 'express',
+        afterRemove: RemoveLastNcharacters('abcdefg')
     });
 });
-function f10(a) {
+
+function RemoveLastNcharacters(a) {
     return a.slice(0, -3);
-  }
-router.get('/f10', function (req, res, next) {
+}
+
+router.get('/percentage-of-number', function (req, res, next) {
     res.send({
-        name: 'f10',
-        sum: f10("1234")
+        name: 'Return the percentage of a number',
+        server: 'express',
+        percentage: percentageOfNumber(500, 25)
     });
 });
 
-function f11(a, b) {
-    return (b / 100) * a;
-  }
-router.get('/f11', function (req, res, next) {
+function percentageOfNumber(a, b) {
+    return b / 100 * a;
+}
+
+router.get('/basic-js-math-operators', function (req, res, next) {
     res.send({
-        name: 'f11',
-        sum: f11(100, 50)
+        name: 'Basic JavaScript math operators',
+        server: 'express',
+        result: basicMathOperators(6, 5, 4, 3, 2, 1)
     });
 });
 
-function f12(a, b, c, d, e, f) {
-    return Math.pow(((a + b - c) * d) / e, f);
-  }
-router.get('/f12', function (req, res, next) {
+function basicMathOperators(a, b, c, d, e, f) {
+    return (((a + b - c) * d) / e) ** f;
+}
+
+router.get('/check-contain-string', function (req, res, next) {
     res.send({
-        name: 'f12',
-        sum: f12(6, 5, 4, 3, 2, 1)
+        name: 'Check whether a string contains another string and concatenate',
+        server: 'express',
+        characters: checkContainString('lips', 's')
     });
 });
 
-function f13(a, b) {
-    return a.includes(b) ? `${b}${a}` : `${a}${b}`;
-  }
-router.get('/f13', function (req, res, next) {
+function checkContainString(a, b) {
+    return a.indexOf(b) === -1 ? a + b : b + a;
+}
+
+router.get('/check-even', function (req, res, next) {
     res.send({
-        name: 'f13',
-        sum: f13("lips", "s")
+        name: 'Check if a number is even',
+        server: 'express',
+        result: checkEven(4)
     });
 });
 
-function f14(a) {
+function checkEven(a) {
     return a % 2 === 0;
-  }
-router.get('/f14', function (req, res, next) {
+}
+
+router.get('/occurrence-times', function (req, res, next) {
     res.send({
-        name: 'f14',
-        sum: f14(10)
-    });
-});
-function f15(a, b) {
-    let characterCount = 0;
-    for (let counter = 0; counter < b.length; counter++) {
-      if (b[counter] === a) {
-        characterCount++;
-      }
-    }
-    return characterCount;
-  }
-router.get('/f15', function (req, res, next) {
-    res.send({
-        name: 'f15',
-        sum: f15("m", "how many times does the character occur in this sentence?")
+        name: 'How many times does a character occur?',
+        server: 'express',
+        result: occurrenceTimes('h', 'how many times does the character occur in this sentence?')
     });
 });
 
-function f16(a) {
-    return Number.isInteger(a);
-  }
-router.get('/f16', function (req, res, next) {
+function occurrenceTimes(a, b) {
+    return b.split(a).length - 1;
+}
+
+router.get('/check-whole-number', function (req, res, next) {
     res.send({
-        name: 'f16',
-        sum: f16(4)
+        name: 'Check if a number is a whole number',
+        server: 'express',
+        result: checkWholeNumber(4.2)
     });
 });
 
-function f17(a, b) {
+function checkWholeNumber(a) {
+    return a - Math.floor(a) === 0;
+}
+
+router.get('/multi-div-compr', function (req, res, next) {
+    res.send({
+        name: 'Multiplication, division, and comparison operators',
+        server: 'express',
+        result: multiDivCompr(90, 45)
+    });
+});
+
+function multiDivCompr(a, b) {
     return a < b ? a / b : a * b;
-  }
-router.get('/f17', function (req, res, next) {
+}
+
+
+router.get('/round-to-two-decimal', function (req, res, next) {
     res.send({
-        name: 'f17',
-        sum: f17(10, 100)
+        name: 'Round a number to 2 decimal places',
+        server: 'express',
+        result: roundToTwoDecimal(2.12397)
     });
 });
 
-function f18(a) {
+function roundToTwoDecimal(a) {
     return Number(a.toFixed(2));
-  }
-router.get('/f18', function (req, res, next) {
+}
+
+router.get('/split-number', function (req, res, next) {
     res.send({
-        name: 'f18',
-        sum: f18(2.12397)
+        name: 'Split a number into its digits',
+        server: 'express',
+        result: splitNumber(193278)
     });
 });
 
-function f19(a) {
-    return String(a)
-      .split("")
-      .map((number) => Number(number));
-  }
-router.get('/f19', function (req, res, next) {
-    res.send({
-        name: 'f19',
-        sum: f19(931)
-    });
-});
+function splitNumber(a) {
+    const string = a + '';
+    const strings = string.split('');
+    return strings.map(digit => Number(digit))
+}
+
 module.exports = router;
